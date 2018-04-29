1e15653fb436539d982b047c9d0218f22b136140
Ibrahim Moustafa <hema.3nany@gmail.com>
2018-04-29 12:04:33 +0200

    able to create post with static category

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -5,6 +5,8 @@
 
     <h1>Create Users</h1>
 
+    @include('includes.form_error')
+
     {!! Form::open(['method'=>'POST','action'=>'AdminUsersController@store','files'=>true]) !!}
 
         <div class="form-group">
@@ -42,7 +44,7 @@
         </div>
     {!! Form::close() !!}
 
-    @include('includes.form_error')
+
 
 
 
